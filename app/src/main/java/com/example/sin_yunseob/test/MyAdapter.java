8657116171e91ba5bcb2c32b293007d862833d81
신윤섭 <sin-yunseob@sin-yunseob-ui-MacBook-Pro.local>
2019-01-25 12:38:14 +0900

    Initial commit 0.0

---
+++ app/src/main/java/com/example/sin_yunseob/test/MyAdapter.java
@@ -0,0 +1,31 @@
+package com.example.sin_yunseob.test;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+
+public class MyAdapter extends RecyclerView.Adapter {
+
+    ArrayList<String> data;
+
+
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+}
