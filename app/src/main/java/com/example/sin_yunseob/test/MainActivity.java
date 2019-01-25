cff217facdb8aba1e41b3d3fcc71a5f9583ee0a5
sin-yunseob <imgenius0136@gmail.com>
2019-01-25 13:29:38 +0900

    0.1

--- app/src/main/java/com/example/sin_yunseob/test/MainActivity.java
+++ app/src/main/java/com/example/sin_yunseob/test/MainActivity.java
@@ -2,12 +2,35 @@ package com.example.sin_yunseob.test;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> data;
+    RecyclerView recyclerView;
+    MyAdapter myAdapter;
+    LinearLayoutManager linearLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        data = new ArrayList<>();
+
+        for(int i = 0 ; i < 10 ; i ++){
+            data.add("recycler" + i);
+        }
+
+        recyclerView = findViewById(R.id.recyclerView);
+        myAdapter = new MyAdapter(data, this);
+
+
+
+        recyclerView.setAdapter(myAdapter);
+
+
     }
 }
--- app/src/main/java/com/example/sin_yunseob/test/MyAdapter.java
+++ app/src/main/java/com/example/sin_yunseob/test/MyAdapter.java
@@ -1,31 +1,67 @@
 package com.example.sin_yunseob.test;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
-public class MyAdapter extends RecyclerView.Adapter {
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
 
     ArrayList<String> data;
+    Context context;
 
-
+    public MyAdapter(ArrayList<String> data, Context context) {
+        this.data = data;
+        this.context = context;
+    }
 
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+    public ViewHolder onCreateViewHolder(@NonNull final ViewGroup viewGroup, int i) {
+        View view = null;
+        view = LayoutInflater.from(context).inflate(R.layout.recycler_item, viewGroup, false);
+        if(view != null){
+            return new ViewHolder(view);
+        }else{
+            return null;
+        }
+
+
+
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
-
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+        ((ViewHolder)viewHolder).textView.setText(data.get(i));
     }
+
 
     @Override
     public int getItemCount() {
-        return 0;
+        return data.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+        TextView textView;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textView = itemView.findViewById(R.id.textView);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            Toast.makeText(context, textView.getText() +"hello", Toast.LENGTH_SHORT).show();
+        }
     }
 
 }
